a42faf9d1b2dded324ee5eda7439fc0eb7eda9fd
baduuz <62142988+baduuz@users.noreply.github.com>
2024-04-07 22:01:37 +0200

    uupdate

--- general/.config/nvim/lua/autocommands.lua
+++ general/.config/nvim/lua/autocommands.lua
@@ -1,18 +1,14 @@
-local yankGroup = vim.api.nvim_create_augroup('YankHighlight', {})
 vim.api.nvim_create_autocmd('TextYankPost', {
-	group = yankGroup,
-    pattern = '*',
+    group = vim.api.nvim_create_augroup('YankHighlight', {}),
     callback = function()
-        vim.highlight.on_yank({
+        vim.highlight.on_yank {
             higroup = 'IncSearch',
             timeout = 40,
-        })
+        }
     end,
 })
 
-local myGroup = vim.api.nvim_create_augroup('MyGroup', {})
 vim.api.nvim_create_autocmd('BufWritePre', {
-    group = myGroup,
-    pattern = '*',
+    group = vim.api.nvim_create_augroup('MyGroup', {}),
     command = [[%s/\s\+$//e]],
 })
