70dd046fa8d143102fd93b61efc0224d6a5c9ddb
baduuz <62142988+baduuz@users.noreply.github.com>
2024-05-02 19:39:48 +0200

    update

--- general/.config/nvim/lua/plugins/treesitter.lua
+++ general/.config/nvim/lua/plugins/treesitter.lua
@@ -5,8 +5,11 @@ return {
     config = function()
         require('nvim-treesitter.configs').setup {
             ensure_installed = { 'c', 'zig', 'java' },
+            ignore_install = { 'latex' },
             auto_install = true,
-            highlight = { enable = true },
+            highlight = {
+                enable = true,
+            },
             indent = { enable = true },
         }
     end,
